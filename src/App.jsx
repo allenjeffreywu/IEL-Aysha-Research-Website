5a41c40d74a4b8e6ee6a439ffa69829bfc3d12b7
Allen Wu <allenwuhoo@gmail.com>
2022-08-23 03:19:58 -0500

    datatable working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
 import Footer from './common/Footer/Footer';
+import BuildingTable from './common/BuildingTable/BuildingTable';
+import ButtonAppBar from './common/AppBar/ButtonAppBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           <code> src/App.js </code>
           and save to reload.
         </p>
-      </header>
-      <Footer name="Your name" />
+      </header> */}
+
+      <ButtonAppBar />
+      <div>
+        <BuildingTable />
+      </div>
+      <Footer name="Allen Wu" />
     </div>
   );
 }
